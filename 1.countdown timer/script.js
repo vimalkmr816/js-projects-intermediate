9ed4d4d9fb6ef110729aadee05782ded641f80f1
vimalkmr816 <vimalkmr816@gmail.com>
2022-01-24 21:05:28 +0530

    removed unnecessory comments

--- 1.countdown timer/script.js
+++ 1.countdown timer/script.js
@@ -1,29 +1,26 @@
 function countdown() {
-    const target = new Date("1 Jan 2023 00:00:00").getTime();
-    // console.log(target);
+    const target = new Date("1 Jan 2023 00:00:00").getTime(); //set the target date
     const now = new Date().getTime();
     const gap = target - now;
-    // console.log(gap);
 
+    //units for conversion of ms to different bases
     const seconds = 1000;
     const minutes = seconds * 60;
     const hours = minutes * 60;
     const days = hours * 24;
-    // const months= days*
 
+    //convert to days, hours, mintues and seconds
     const textDays = Math.floor(gap / days);
     const textHours = Math.floor((gap % days) / hours);
     const textMinutes = Math.floor((gap % hours) / minutes);
     const textSeconds = Math.floor((gap % minutes) / seconds);
-    // const textSeconds = Math.floor((gap % minutes) / seconds);
-    // console.log(textDays);
-    // console.log(textHours);
-    // console.log(textMinutes);
-    // console.log(textSeconds);
+
+    //update html elements
     document.getElementById('seconds').innerHTML = textSeconds;
     document.getElementById('minutes').innerHTML = textMinutes;
     document.getElementById('hours').innerHTML = textHours;
     document.getElementById('days').innerHTML = textDays;
-    // document.getElementById('months').innerHTML = textSeconds;
 }
+
+//update the elements every 1000ms(1s) 
 setInterval(countdown, 1000);
